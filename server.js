f6c37415d9d4f8df3ff67c3a4155c34c5cb59858
Sobhan <sobhan_Makki@yahoo.com>
2018-07-24 01:47:35 +0430

    logger

--- server.js
+++ server.js
@@ -1,18 +1,52 @@
 // Setup
-var express = require('express');
-var app = express();
-var mongoose = require('mongoose')
+var express = require('express'),
+    app = express(),
+    bodyParser = require('body-parser'),
+    methodOverride = require('method-override'),
+    logger = require('morgan'),
+    mongoose = require('mongoose');
 
-mongoose.connect("mongodb://localhost:27017/blog", { useNewUrlParser: true })
+var morgan = require('morgan');
+var fs = require('fs');
+var path = require('path');
+var rfs = require('rotating-file-stream')
 
-var bodyParser = require('body-parser')
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({
-    extended: true
-}))
+
+mongoose.connect("mongodb://localhost:27017/blog", {
+    useNewUrlParser: true
+}, () => {
+    console.log('We are connected to MongoDB !');
+});
+
+// var bodyParser = require('body-parser')
 
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
+
+app.use(express.static('public')); // added to project according to tutorial
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+
+// set up our express application - logger
+var logDirectory = path.join(__dirname, 'logger') //logDirectory
+// ensure log directory exists
+fs.existsSync(logDirectory) || fs.mkdirSync(logDirectory)
+// create a rotating write stream
+var accessLogStream = rfs('logger.csv', {
+    interval: '1d', // rotate daily
+    path: logDirectory
+})
+// setup the logger
+app.use(morgan(':remote-addr, :remote-user, [:date[web]], :method, :url, HTTP/:http-version, :response-time[digits], :status, :res[content-length], :req[header], :res[header], ":referrer", ":user-agent"', {
+    stream: accessLogStream
+}))
+// show in commad window
+app.use(logger('dev'));
+
+
+
 
 var postSchema = new mongoose.Schema({
     body: String
@@ -41,5 +75,5 @@ app.post('/addpost', (req, res) => {
 
 // Listen
 app.listen(8080, () => {
-    console.log('\nServer is listening on 8080');
+    console.log('\nServer is Running on port 8080');
 })
