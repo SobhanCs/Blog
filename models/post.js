babfe42f71f86720aa607224e2bc0798614b336d
Sobhan <seid.sobhan.maki@gmail.com>
2018-07-24 08:34:32 +0430

    Update post.js

--- models/post.js
+++ models/post.js
@@ -3,6 +3,9 @@ var mongoose = require('mongoose');
 var postSchema = new mongoose.Schema({
     title: {type: String, required: true},
     image: {type: String, required: true},
+    star: {type: String},
+    seen: {type: String},
+    date: {type: String},
     author: {
         id: {type: mongoose.Schema.types.ObjectId, ref: 'User'},
         username: {type: String, required: true}
